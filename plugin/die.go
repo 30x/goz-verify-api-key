e8d2f32b9587da91edf44d12627071f245892158
Scott Ganyo <scott@ganyo.com>
2016-07-11 15:14:44 -0700

    match the documented apid spec, cleanup

--- plugin/die.go
+++ plugin/die.go
@@ -1,28 +1,47 @@
 /*
-This example Gozerian plugin validates an API Key passed in a header by contacting the Apigee Edge Micro proxy on the server. If the API Key is missing the client receives a 401. If the API Key is incorrect, the client will receive a 403. If the API Key is valid, the key will be deleted from the header, the data from the server will be stored in the control.FlowData and the request will continue.
+A Gozerian plugin that validates an API Key passed in a header by contacting the localhost apid server. If the API Key
+is valid, the key will be deleted from the header, the data from the server will be stored in the control.FlowData and
+the request will continue.
 
 Sample Configuration:
 
-	proxyURI:     https://sganyo-test.apigee.net/remoteproxy/v2/oauth/verifyApiKey
-	proxyKey:     customerKeyForTheProxy
-	apiKeyHeader: X-Apigee-API-Key
+	keyHeader: X-Apigee-API-Key
+	organization: test
+	environment: test
 */
 package plugin
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/30x/gozerian/pipeline"
 	"io/ioutil"
 	"net/http"
-	"regexp"
-
-	"github.com/30x/gozerian/pipeline"
 )
 
-const VALID_URL string = `^((https?):\/\/)?(\S+(:\S*)?@)?((([1-9]\d?|1\d\d|2[01]\d|22[0-3])(\.(1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.([0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(([a-zA-Z0-9]+([-\.][a-zA-Z0-9]+)*)|((www\.)?))?(([a-zA-Z\x{00a1}-\x{ffff}0-9]+-?-?)*[a-zA-Z\x{00a1}-\x{ffff}0-9]+)(?:\.([a-zA-Z\x{00a1}-\x{ffff}]{1,}))?))(:(\d{1,5}))?((\/|\?|#)[^\s]*)?$`
+const APID_URI = "http://localhost:8081/verifiers/apikey"
 
-//CreateFitting exported function to create the fitting
+type verifyAPIKeyConfig struct {
+	keyHeader    string
+	organization string
+	environment  string
+}
+
+type verifyAPIKeyFitting struct {
+	config verifyAPIKeyConfig
+}
+
+type verifyAPIBody struct {
+	action       string
+	organization string
+	environment  string
+	key          string
+	uriPath      string
+}
+
+// CreateFitting exported function to create the fitting
 func CreateFitting(config interface{}) (pipeline.Fitting, error) {
 
 	conf, ok := config.(map[interface{}]interface{})
@@ -31,38 +50,24 @@ func CreateFitting(config interface{}) (pipeline.Fitting, error) {
 	}
 
 	c := verifyAPIKeyConfig{
-		apidURL:      conf["apidURL"].(string),
-		apiKeyHeader: conf["apiKeyHeader"].(string),
-		apiOrg:       conf["apiOrg"].(string),
+		keyHeader:    conf["keyHeader"].(string),
+		organization: conf["organization"].(string),
+		environment:  conf["environment"].(string),
 	}
 
-	match, err := regexp.Match(VALID_URL, []byte(c.apidURL))
-	if err != nil {
-		return nil, err
-	}
-	if !match {
-		return nil, fmt.Errorf("invalid proxyURI: %s", c.apidURL)
+	if c.keyHeader == "" {
+		return nil, fmt.Errorf("invalid config: missing keyHeader")
 	}
 
-	if c.apiKeyHeader == "" {
-		return nil, fmt.Errorf("invalid config: missing apiKeyHeader")
+	if c.organization == "" {
+		return nil, fmt.Errorf("invalid config: missing organization")
 	}
 
-	if c.apiOrg == "" {
-		return nil, fmt.Errorf("invalid config: missing apiOrg")
+	if c.organization == "" {
+		return nil, fmt.Errorf("invalid config: missing environment")
 	}
 
 	return &verifyAPIKeyFitting{c}, nil
-}
-
-type verifyAPIKeyConfig struct {
-	apidURL      string
-	apiKeyHeader string
-	apiOrg       string
-}
-
-type verifyAPIKeyFitting struct {
-	config verifyAPIKeyConfig
 }
 
 func (f *verifyAPIKeyFitting) RequestHandlerFunc() http.HandlerFunc {
@@ -71,7 +76,7 @@ func (f *verifyAPIKeyFitting) RequestHandlerFunc() http.HandlerFunc {
 		control := w.(pipeline.ControlHolder).Control()
 		log := control.Log()
 
-		apiKey := r.Header.Get(f.config.apiKeyHeader)
+		apiKey := r.Header.Get(f.config.keyHeader)
 
 		log.Debugf("Api config is %v.  Header value is %s. \n", f.config, apiKey)
 
@@ -86,19 +91,31 @@ func (f *verifyAPIKeyFitting) RequestHandlerFunc() http.HandlerFunc {
 
 		//create our request
 		client := &http.Client{}
-		req, err := http.NewRequest("GET", f.config.apidURL, nil)
+
+		msg := verifyAPIBody{
+			action: "verify",
+			"organization", f.config.organization,
+			"environment", f.config.environment,
+			"key", apiKey,
+			"uriPath": path,
+		}
+		body, err := json.Marshal(msg)
+		if err != nil {
+			log.Debugf("error creating request body: %s\n", err.Error())
+			control.SendError(err)
+			return
+		}
+
+		req, err := http.NewRequest("POST", APID_URI, bytes.NewBuffer(body))
 		if err != nil {
 			log.Debugf("error creating request: %s\n", err.Error())
 			control.SendError(err)
 			return
 		}
 
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("apiKey", apiKey)
-		req.Header.Set("org", f.config.apiOrg)
-		req.Header.Set("path", path)
+		log.Debugf("Posting %s with body:\n", req.URL.String(), body)
 
-		log.Debugf("Requesting %s with apiKey:%s org:%s path:%s \n", req.URL.String(), apiKey, f.config.apiOrg, path)
+		req.Header.Set("Content-Type", "application/json")
 
 		res, err := client.Do(req)
 		if err != nil {
@@ -116,18 +133,14 @@ func (f *verifyAPIKeyFitting) RequestHandlerFunc() http.HandlerFunc {
 			return
 		}
 
-		//if we get a forbidden, pass it along.  Any other error will be sent as well
+		// if we get a forbidden, pass it along.  Any other error will be sent as well
 		if res.StatusCode < 200 || res.StatusCode >= 300 {
 			log.Debugf("error from server on key verification: %s\n", string(resBytes))
 			control.SendError(fmt.Errorf("error from server on key verification: %s (%d)", string(resBytes), res.StatusCode))
 			return
 		}
 
-		//TODO, do we want to delete the header?
-		r.Header.Del(f.config.apiKeyHeader)
-
-		//Take everything from the valid resposne and pass it along.
-		//TODO, should the keys be namespaced so it's clear what plugin has populated them?
+		// Take everything from the valid response and pass it along.
 		newFlowVars := make(map[string]interface{})
 		err = json.Unmarshal(resBytes, &newFlowVars)
 		if err != nil {
@@ -147,7 +160,3 @@ func (f *verifyAPIKeyFitting) RequestHandlerFunc() http.HandlerFunc {
 func (f *verifyAPIKeyFitting) ResponseHandlerFunc() pipeline.ResponseHandlerFunc {
 	return nil
 }
-
-/* This is what a valid result from validate looks like:
-{"developer_email":"remote-proxy@apigee.com","issued_at":1446155872425,"status":"approved","apiproduct_name":"remoteproxy","developer_app_id":"12a6600f-18ef-46a2-bd50-d3b529d18d68","expires_in":0,"client_id":"qFj141vqfH3dGyYv6useAmsxYWKeuCtO","developer_id":"sganyo@@@80c9db9b-8e2d-463f-90f4-988a8ad865f9","developer_app_name":"remoteproxy","attributes":{"client_secret":"YyAcpkSDHH7X8jXj","redirection_uris":null},"developer":{"apps":["remoteproxy"],"app_name":"remoteproxy","app_id":"12a6600f-18ef-46a2-bd50-d3b529d18d68","id":"sganyo@@@80c9db9b-8e2d-463f-90f4-988a8ad865f9","attributes":{"created_by":"sganyo@apigee.com","lastName":"Proxy","last_modified_by":"sganyo@apigee.com","last_modified_at":"1421284658925","status":"active","email":"remote-proxy@apigee.com","created_at":"1421284658925","userName":"remote-proxy","firstName":"Remote"}},"app":{"apiproducts":["remoteproxy"],"scopes":[],"attributes":{"created_by":"sganyo@apigee.com","id":"12a6600f-18ef-46a2-bd50-d3b529d18d68","callbackUrl":"","last_modified_by":"sganyo@apigee.com","last_modified_at":"1446155872242","status":"approved","appParentId":"sganyo@@@80c9db9b-8e2d-463f-90f4-988a8ad865f9","appParentStatus":"active","name":"remoteproxy","created_at":"1446155872242","appFamily":"default","appType":"Developer","accessType":null}}}
-*/
