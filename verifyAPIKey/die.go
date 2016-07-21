507f8feb1434686a479d74864bb935990e34e5ab
Scott Ganyo <scott@ganyo.com>
2016-07-21 07:30:55 -0700

    Wrap apid call in hystrix, add send404OnError option

--- verifyAPIKey/die.go
+++ verifyAPIKey/die.go
@@ -22,16 +22,19 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"github.com/afex/hystrix-go/hystrix"
 )
 
 const DEFAULT_APID_URI = "http://localhost:8181/verifiers/apikey"
 const DEFAULT_KEY_HEADER = "X-Apigee-API-Key"
+const UNAUTHORIZED_MESSAGE = "Unauthorized"
 
 type verifyAPIKeyConfig struct {
 	apidUri      string
 	keyHeader    string
 	organization string
 	environment  string
+	send404OnError bool
 }
 
 type verifyAPIKeyFitting struct {
@@ -64,27 +67,34 @@ func CreateFitting(config interface{}) (pipeline.Fitting, error) {
 		return nil, errors.New("Invalid config. Expected map[interface{}]interface{}")
 	}
 
-	c := verifyAPIKeyConfig{
-		apidUri:      conf["apidUri"].(string),
-		keyHeader:    conf["keyHeader"].(string),
-		organization: conf["organization"].(string),
-		environment:  conf["environment"].(string),
-	}
+	// validate all config entries
 
-	if c.apidUri == "" {
-		c.apidUri = DEFAULT_APID_URI
-	}
-
-	if c.keyHeader == "" {
-		c.keyHeader = DEFAULT_KEY_HEADER
-	}
-
-	if c.organization == "" {
+	if conf["organization"] == nil {
 		return nil, fmt.Errorf("invalid config: missing organization")
 	}
 
-	if c.environment == "" {
+	if conf["environment"] == nil {
 		return nil, fmt.Errorf("invalid config: missing environment")
+	}
+
+	if conf["apidUri"] == nil {
+		conf["apidUri"] = DEFAULT_APID_URI
+	}
+
+	if conf["keyHeader"] == nil {
+		conf["keyHeader"] = DEFAULT_KEY_HEADER
+	}
+
+	if conf["send404OnError"] == nil {
+		conf["send404OnError"] = false
+	}
+
+	c := verifyAPIKeyConfig{
+		apidUri:        conf["apidUri"].(string),
+		keyHeader:      conf["keyHeader"].(string),
+		organization:   conf["organization"].(string),
+		environment:    conf["environment"].(string),
+		send404OnError: conf["send404OnError"].(bool),
 	}
 
 	return &verifyAPIKeyFitting{c}, nil
@@ -101,8 +111,12 @@ func (f *verifyAPIKeyFitting) RequestHandlerFunc() http.HandlerFunc {
 		log.Debugf("Api config is %v.  Header value is %s. \n", f.config, apiKey)
 
 		if apiKey == "" {
-			w.WriteHeader(401)
-			w.Write([]byte("Unauthorized"))
+			if f.config.send404OnError {
+				w.WriteHeader(http.StatusNotFound)
+			} else {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte(UNAUTHORIZED_MESSAGE))
+			}
 			return
 		}
 
@@ -121,67 +135,76 @@ func (f *verifyAPIKeyFitting) RequestHandlerFunc() http.HandlerFunc {
 			return
 		}
 
-		//req.Header.Set("Content-Type", "application/json")
 		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-		client := &http.Client{}
-		res, err := client.Do(req)
-		if err != nil {
-			log.Debugf("error getting from server: %s\n", err.Error())
-			control.SendError(err)
-			return
-		}
-		defer res.Body.Close()
+		err = hystrix.Do(DEFAULT_APID_URI, func() error {
 
-		resBytes, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			log.Debugf("error reading reply: %s\n", err.Error())
-			control.SendError(err)
-			return
-		}
-
-		// if we get a forbidden, pass it along.  Any other error will be sent as well
-		if res.StatusCode < 200 || res.StatusCode >= 300 {
-			log.Debugf("key verification error: %s\n", string(resBytes))
-			control.SendError(fmt.Errorf("key verification error: %s (%d)", string(resBytes), res.StatusCode))
-			return
-		}
-
-		// check for failed
-		failResponse := KMSResponseFail{}
-		err = json.Unmarshal(resBytes, &failResponse)
-		if err != nil {
-			control.SendError(err)
-			return
-		}
-		if failResponse.Errinfo.ErrorCode != "" {
-			msg, _ := json.Marshal(failResponse.Errinfo)
-			// TODO: this needs to be revisited!!!
-			// 1. the response code should not be dictated by apid
-			// 2. should it return http.StatusUnauthorized? or 404? should this be an option?
-			respCode := failResponse.ResponseCode
-			if respCode == 404 {
-				respCode = http.StatusUnauthorized
+			client := &http.Client{}
+			res, err := client.Do(req)
+			if err != nil {
+				log.Debugf("error getting from server: %s\n", err.Error())
+				control.SendError(err)
+				return nil
 			}
-			w.WriteHeader(respCode)
-			w.Write([]byte(msg))
-			return
-		}
+			defer res.Body.Close()
 
-		// store valid response in flowdata
-		response := KMSResponseSuccess{}
-		err = json.Unmarshal(resBytes, &response)
+			resBytes, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				log.Debugf("error reading reply: %s\n", err.Error())
+				control.SendError(err)
+				return nil
+			}
+
+			// if we get an error, log and send 500 to client
+			if res.StatusCode < 200 || res.StatusCode >= 300 {
+				if f.config.send404OnError {
+					w.WriteHeader(http.StatusNotFound)
+				} else {
+					log.Debugf("key verification code: %d message: %s\n", res.StatusCode, string(resBytes))
+					control.SendError(fmt.Errorf("internal error with key verification"))
+				}
+				return nil
+			}
+
+			// check for failed
+			failResponse := KMSResponseFail{}
+			err = json.Unmarshal(resBytes, &failResponse)
+			if err != nil {
+				control.SendError(err)
+				return nil
+			}
+			if failResponse.Errinfo.ErrorCode != "" {
+				log.Debugf("key verification failed: %s\n", failResponse.Errinfo)
+				if f.config.send404OnError {
+					w.WriteHeader(http.StatusNotFound)
+				} else {
+					w.WriteHeader(http.StatusForbidden)
+					w.Write([]byte(UNAUTHORIZED_MESSAGE))
+				}
+				return nil
+			}
+
+			// store valid response in flowdata
+			response := KMSResponseSuccess{}
+			err = json.Unmarshal(resBytes, &response)
+			if err != nil {
+				control.SendError(err)
+				return nil
+			}
+			flowData := control.FlowData()
+			for k, v := range response.Rspinfo {
+				flowData[k] = v
+				log.Debugf("Setting flow var: %s to: %v\n", k, v)
+			}
+
+			log.Debugln("Successful validation!")
+
+			return nil
+		}, nil)
+
 		if err != nil {
 			control.SendError(err)
-			return
 		}
-		flowData := control.FlowData()
-		for k, v := range response.Rspinfo {
-			flowData[k] = v
-			log.Debugf("Setting flow var: %s to: %v\n", k, v)
-		}
-
-		log.Debugln("Successful validation!")
 	}
 }
 
